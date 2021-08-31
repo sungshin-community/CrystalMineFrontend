cb2d7a7a281e5d4e6c9ac59dd4bd633c9e820ea2
feel-coding <46566478+feel-coding@users.noreply.github.com>
2021-08-31 19:46:58 +0900

    프로젝트명 변경, 타입스크립트로 변경
    #1

--- android/app/src/main/java/com/crystalminefrontend/MainActivity.java
+++ android/app/src/main/java/com/crystalminefrontend/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativetest;
+package com.community.sungshin;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeTest";
+    return "CrystalMineFrontend";
   }
 }
