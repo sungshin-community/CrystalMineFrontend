8a72cd83015aab3dc96a16e3ec1649f8d144e6f4
bgyoons <gye04056@gmail.com>
2021-09-24 01:59:36 +0900

    #6: splash

--- App.tsx
+++ App.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TermAgree from './src/screens/signUp/TermAgree';
 import Home from './src/screens/Home';
-
-import {
-  StatusBar
-} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import {StatusBar} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  StatusBar.setBackgroundColor("white");
+  StatusBar.setBackgroundColor('white');
   // StatusBar.setTranslucent(true);
   StatusBar.setBarStyle('dark-content');
 
+  // useEffect(() => {
+  //   SplashScreen.hide();
+  // }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShadowVisible: false }}>
+      <Stack.Navigator screenOptions={{headerShadowVisible: false}}>
+        <Stack.Screen name="Home" component={Home} options={{title: ''}} />
         <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: '' }}
+          name="Profile"
+          component={TermAgree}
+          options={{title: ''}}
         />
-        <Stack.Screen name="Profile" component={TermAgree} options={{ title: '' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
