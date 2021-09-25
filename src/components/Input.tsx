af784c09846cb3fa7cc93fa32b20ffec38b3ca2e
bgyoons <gye04056@gmail.com>
2021-09-25 22:43:49 +0900

    #8: input Component Focus 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import {View, Text, TextInput, Keyboard, ScrollView} from 'react-native';
+import {
+  DisabledPurpleRoundButton,
+  PurpleFullButton,
+  DisabledPurpleFullButton,
+  PurpleRoundButton,
+} from './Button';
 
 const MiddleFocusInputStyle = styled.TextInput`
   width: 100%;
@@ -74,10 +81,235 @@ const CautionTextStyle = styled.Text`
   color: #e64646;
   margin: 10px 0;
 `;
+
+const MiddleInputContainerStyle = styled.View`
+  font-size: 21px;
+  border-bottom-width: 2px;
+  flex-direction: row;
+  align-items: center;
+`;
+
 interface Props {
   placeholder: string;
   title: string;
+  maxLength: number;
+  keyboardType: any;
+  suffix: string;
 }
+
+export const MiddleActiveInputID = ({
+  placeholder,
+  maxLength,
+  keyboardType,
+  suffix,
+}: Props) => {
+  const [studentId, setStudentId] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const onInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const onInputFocusOut = () => {
+    setIsFocused(false);
+    Keyboard.dismiss();
+  };
+  return (
+    <>
+      <ScrollView
+        scrollEnabled={false}
+        keyboardShouldPersistTaps="handled"
+        style={{backgroundColor: '#fff', marginHorizontal: 24}}>
+        <MiddleInputContainerStyle
+          style={{
+            borderColor: isFocused ? '#A055FF' : '#D7DCE6',
+          }}>
+          <TextInput
+            style={{width: '60%'}}
+            onFocus={(e: any) => {
+              onInputFocus();
+            }}
+            onBlur={(e: any) => {
+              onInputFocusOut();
+            }}
+            onChangeText={(value: string) => {
+              setStudentId(value);
+            }}
+            maxLength={maxLength}
+            placeholder={placeholder}
+            placeholderTextColor="#A0AAB4"
+            keyboardType={keyboardType}
+          />
+          <Text
+            style={{
+              fontSize: 15,
+              paddingLeft: 10,
+              color: '#87919B',
+              textAlign: 'right',
+            }}>
+            {suffix}
+          </Text>
+        </MiddleInputContainerStyle>
+      </ScrollView>
+      <View
+        style={{
+          bottom: isFocused ? 0 : 21,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {studentId.length === 8 && isFocused && (
+          <PurpleFullButton text="다음" />
+        )}
+
+        {studentId.length === 8 && !isFocused && (
+          <PurpleRoundButton text="다음" />
+        )}
+
+        {studentId.length < 8 && isFocused && (
+          <DisabledPurpleFullButton text="다음" />
+        )}
+
+        {studentId.length < 8 && !isFocused && (
+          <DisabledPurpleRoundButton text="다음" />
+        )}
+      </View>
+    </>
+  );
+};
+
+export const MiddleActiveInputPassword = ({
+  placeholder,
+  maxLength,
+  keyboardType,
+}: Props) => {
+  const [password, setPassword] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const onInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const onInputFocusOut = () => {
+    setIsFocused(false);
+    Keyboard.dismiss();
+  };
+  return (
+    <>
+      <ScrollView
+        scrollEnabled={false}
+        keyboardShouldPersistTaps="handled"
+        style={{backgroundColor: '#fff', marginHorizontal: 24}}>
+        <MiddleInputContainerStyle
+          style={{
+            borderColor: isFocused ? '#A055FF' : '#D7DCE6',
+          }}>
+          <TextInput
+            style={{width: '60%'}}
+            onFocus={(e: any) => {
+              onInputFocus();
+            }}
+            onBlur={(e: any) => {
+              onInputFocusOut();
+            }}
+            onChangeText={(value: string) => {
+              setPassword(value);
+            }}
+            maxLength={maxLength}
+            placeholder={placeholder}
+            placeholderTextColor="#A0AAB4"
+            keyboardType={keyboardType}
+            secureTextEntry={true}
+          />
+        </MiddleInputContainerStyle>
+      </ScrollView>
+      <View
+        style={{
+          bottom: isFocused ? 0 : 21,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {password.length >= 10 && password.length <= 25 && isFocused && (
+          <PurpleFullButton text="다음" />
+        )}
+
+        {password.length >= 10 && password.length <= 25 && !isFocused && (
+          <PurpleRoundButton text="다음" />
+        )}
+
+        {password.length < 10 && isFocused && (
+          <DisabledPurpleFullButton text="다음" />
+        )}
+
+        {password.length < 10 && !isFocused && (
+          <DisabledPurpleRoundButton text="다음" />
+        )}
+      </View>
+    </>
+  );
+};
+
+export const MiddleActiveInputNickname = ({
+  placeholder,
+  keyboardType,
+}: Props) => {
+  const [nickname, setNickname] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const onInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const onInputFocusOut = () => {
+    setIsFocused(false);
+    Keyboard.dismiss();
+  };
+  return (
+    <>
+      <ScrollView
+        scrollEnabled={false}
+        keyboardShouldPersistTaps="handled"
+        style={{backgroundColor: '#fff', marginHorizontal: 24}}>
+        <MiddleInputContainerStyle
+          style={{
+            borderColor: isFocused ? '#A055FF' : '#D7DCE6',
+          }}>
+          <TextInput
+            style={{width: '60%'}}
+            onFocus={(e: any) => {
+              onInputFocus();
+            }}
+            onBlur={(e: any) => {
+              onInputFocusOut();
+            }}
+            onChangeText={(value: string) => {
+              setNickname(value);
+            }}
+            placeholder={placeholder}
+            placeholderTextColor="#A0AAB4"
+            keyboardType={keyboardType}
+          />
+        </MiddleInputContainerStyle>
+      </ScrollView>
+      <View
+        style={{
+          bottom: isFocused ? 0 : 21,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {nickname.length !== 0 && isFocused && <PurpleFullButton text="다음" />}
+
+        {nickname.length !== 0 && !isFocused && (
+          <PurpleRoundButton text="다음" />
+        )}
+
+        {nickname.length === 0 && isFocused && (
+          <DisabledPurpleFullButton text="다음" />
+        )}
+
+        {nickname.length === 0 && !isFocused && (
+          <DisabledPurpleRoundButton text="다음" />
+        )}
+      </View>
+    </>
+  );
+};
 
 export const MiddleFocusInput = ({placeholder, title}: Props) => {
   return (
