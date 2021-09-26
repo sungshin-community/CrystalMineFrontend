f7e1783362f145d4d88287df5861f56c3de9800b
YANG <ynawhocodes@gmail.com>
2021-09-26 19:00:41 +0900

    #4 화면 간 연동 전 최종 merge

---
+++ src/screens/signUp/RegularMemberAuth.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  GestureResponderEvent,
+  Button,
+  KeyboardAvoidingView,
+} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
+import CustomButton, {
+  WhiteRoundButton,
+  PurpleRoundButton,
+  DisabledPurpleRoundButton,
+  PurpleFullButton,
+  DisabledWhiteRoundButton,
+  DisabledPurpleFullButton
+} from '../../components/Button';
+import { BigTwoLineText, TwoLineTitle, Description, SmallText, NormalText } from '../../components/Top';
+
+
+import { Container } from '../../components/Container';
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CountDownTimer from '../../components/CountDownTimer';
+import AuthInput from '../../components/AuthInput'
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 50,
+        paddingLeft: 10,
+        paddingRight: 10,
+      },
+
+});
+
+const RegularMemberAuth = () => {
+  return (
+    
+    <View>
+        <View style={{ marginTop: 130}}>
+            <Container>
+            <TwoLineTitle firstLineText="메일로 전송된" secondLineText="인증번호를 입력해주세요"></TwoLineTitle>
+            </Container>
+            <View style={styles.container} >
+                <AuthInput></AuthInput>
+            </View>
+            <CountDownTimer minutes={3} seconds={0}/>
+            
+        </View>
+
+      <KeyboardAvoidingView behavior="padding" >   
+        <View style={{ position: 'absolute', flex: 1}}>
+          <DisabledPurpleFullButton text='인증 완료' />   
+        </View> 
+      </KeyboardAvoidingView>
+
+    </View>
+    
+  );
+};
+export default RegularMemberAuth;
