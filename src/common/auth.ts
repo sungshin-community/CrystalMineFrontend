d4e2df4fcc500a91f29d97390843c285e46e21fd
Baeg <gye04056@gmail.com>
2021-12-18 04:11:12 +0900

    push

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -1,12 +1,26 @@
-import SignUpDto from "../classes/SignUpDto";
-import client from "./api";
+import SignUpDto from '../classes/SignUpDto';
+import client from './api';
 import axios from 'axios';
 
-export const signUp = ({userName, password, nickname, agreementIds, departmentId}: SignUpDto) => {
-    return client.post("/auth/signup", {username: userName, password: password, nickname: nickname, agreementIds: agreementIds, departmentId: departmentId})
+export const signUp = ({
+  userName,
+  password,
+  nickname,
+  agreementIds,
+  departmentId,
+}: SignUpDto) => {
+  return client
+    .post('/auth/signup', {
+      username: userName,
+      password: password,
+      nickname: nickname,
+      agreementIds: agreementIds,
+      departmentId: departmentId,
+    })
     .then(function (response) {
-        console.log(response);
-    }).catch(function (error) {
-        console.log(error);
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-}
+};
