988c15f8b0fb915198fbe56a3667242302300ffe
Feel Coding <khe628@naver.com>
2022-01-02 03:53:02 +0900

    #43 변경된 디자인 적용

--- src/components/HideToggleContainer.tsx
+++ src/components/HideToggleContainer.tsx
@@ -22,13 +22,26 @@ export function BoardListContainer({boardCategory, component}: Props) {
     return (
         <>
             <TouchableOpacity
-                style={{flexDirection: 'row', paddingLeft: 25, paddingVertical: 16, height: 54, alignItems: 'center', backgroundColor: '#FFFFFF', marginTop: 10}}
+                style={{
+                  flexDirection: 'row', 
+                  paddingLeft: 25, 
+                  paddingVertical: 24, 
+                  height: 68, 
+                  alignItems: 'center', 
+                  backgroundColor: '#F6F6F6', 
+                  marginTop: 10,
+                  borderTopLeftRadius: 16,
+                  borderTopRightRadius: 16,
+                  borderBottomLeftRadius: !isSpread ? 16 : 0,
+                  borderBottomRightRadius: !isSpread ? 16 : 0
+                }}
                 onPress={() => setIsSpread(!isSpread)}
             >
                 <Text style={{fontSize: 16, fontWeight: '500', lineHeight: 20, flex: 1}}>{boardCategory}</Text>
                 {isSpread ? <BigFoldButton style={{marginRight: 30}} /> : <BigSpreadButton style={{marginRight: 30}} />}
             </TouchableOpacity>
-            {isSpread && <View style={{flexBasis: 'auto'}}>{component}</View>}
+            {isSpread && <><View style={{flexBasis: 'auto'}}>{component}</View><TouchableOpacity style={{height: 17, backgroundColor: '#F6F6F6', borderBottomRightRadius: 16, borderBottomLeftRadius: 16}} /></>}
+
         </>
     )
 }
