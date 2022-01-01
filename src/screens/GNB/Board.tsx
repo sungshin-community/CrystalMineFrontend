9885238ebadda1a3a501896105e7283f60db12d2
Feel Coding <khe628@naver.com>
2022-01-02 05:03:05 +0900

    #47 게시판 탭 전체화면 차지하게 함

--- src/screens/GNB/Board.tsx
+++ src/screens/GNB/Board.tsx
@@ -4,12 +4,13 @@ import BoardScreen from '../board/BoardScreen';
 
 const Board = () => {
   return (
-    // <SafeAreaView>
-    //   <View style={{alignItems: 'center'}}>
-    //     <Text>board page</Text>
-    //   </View>
-    // </SafeAreaView>
-    <BoardScreen />
+    <SafeAreaView style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+      {/* <View style={{alignItems: 'center'}}>
+        <Text>board page</Text>
+      </View> */}
+      <BoardScreen />
+    </SafeAreaView>
+    
   );
 };
 
