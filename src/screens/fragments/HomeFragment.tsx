21ee8a6977a8343534b54ec22d99f20f6da5a39f
Feel Coding <khe628@naver.com>
2022-01-03 01:38:14 +0900

    #50 홈 화면에 닉네임 포함 인삿말 추가

--- src/screens/fragments/HomeFragment.tsx
+++ src/screens/fragments/HomeFragment.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, View, ScrollView} from 'react-native';
 import { sendEmail } from '../../common/authApi';
 import { PurpleRoundButton } from '../../components/Button';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeFragment = () => {
+  const [nickname, setNickname] = useState<string>('익명');
+  useEffect(() => {
+    async function getNickname() {
+      const nickname = await AsyncStorage.getItem("nickname");
+      if (nickname != null) {
+        setNickname(nickname);
+      }
+    }
+    getNickname();
+    
+  }, []);
+
   return (
-    <SafeAreaView>
-      <View style={{alignItems: 'center'}}>
-        <Text>home page</Text>
-        <PurpleRoundButton text='정회원 인증받기' onClick={async () => {
-          await sendEmail();
-        }} />
+    <ScrollView style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+      <View>
+        <View style={{ backgroundColor: '#F7F7F7', marginHorizontal: 12, borderRadius: 20, marginTop: 12}}>
+          <View style={{ borderRadius: 20, marginLeft: 32, marginTop: 32, marginBottom: 32}}><Text style={{fontSize: 22}}>{`${nickname} 님, 안녕하세요!`}</Text></View>
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
