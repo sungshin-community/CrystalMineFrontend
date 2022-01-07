4e2a06cb6d8ba2469afb4e9df37efbfcd9a467cb
yanwhocodes <ynawhocodes@gmail.com>
2022-01-07 23:52:06 +0900

    #58 SplashHome.tsx 복구

--- src/screens/SplashHome.tsx
+++ src/screens/SplashHome.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import styled from 'styled-components/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {PurpleRoundButton, WhiteRoundButton} from '../components/Button';
 import Logo from '../../resources/icon/Logo';
 import * as Animatable from 'react-native-animatable';
-
-import {ModalBottom} from '../components/ModalBottom';
 
 type RootStackParamList = {
   TermAgree: undefined;
@@ -31,10 +29,25 @@ type Props = NativeStackScreenProps<RootStackParamList>;
 const SplashHome = ({navigation}: Props) => {
   return (
     <>
-      <ModalBottom
-        modalText="안녕하세요"
-        modalButtonText="확인"
-        modalButton={<Text>모달버튼</Text>}></ModalBottom>
+      <Container>
+        <Animatable.Text animation="fadeIn" duration={2000}>
+          <Logo />
+        </Animatable.Text>
+      </Container>
+      <ButtonContainer>
+        <Animatable.View animation="fadeInUp" delay={2000} duration={1200}>
+          <PurpleRoundButton
+            text="로그인"
+            onClick={() => navigation.navigate('SignInId')}
+          />
+          <View style={{marginTop: 16}}>
+            <WhiteRoundButton
+              text="회원가입"
+              onClick={() => navigation.navigate('TermAgree')}
+            />
+          </View>
+        </Animatable.View>
+      </ButtonContainer>
     </>
   );
 };
