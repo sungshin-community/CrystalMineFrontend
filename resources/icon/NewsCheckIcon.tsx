c7417dceb521ba37e70c0ddc5214cc5485f9044a
yanwhocodes <ynawhocodes@gmail.com>
2022-01-10 01:58:41 +0900

    #71 홈 고정게시판 목록 완성

---
+++ resources/icon/NewsCheckIcon.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Svg, {SvgProps, Path, Circle} from 'react-native-svg';
+
+const NewsCheckIcon = (props: any) => (
+  <Svg
+    width="30"
+    height="30"
+    viewBox="0 0 30 30"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}>
+    <Circle cx="15" cy="15" r="15" fill="#FFA767" />
+    <Path
+      d="M7.5 15.7L12 20.8333L22.5 9.16666"
+      stroke="white"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </Svg>
+);
+
+export default NewsCheckIcon;
--- src/screens/fragments/HomeFragment.tsx
+++ src/screens/fragments/HomeFragment.tsx
@@ -1,27 +1,106 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text, View, ScrollView} from 'react-native';
-import { sendEmail } from '../../common/authApi';
-import { PurpleRoundButton } from '../../components/Button';
+import {SafeAreaView, Text, View, ScrollView, StyleSheet} from 'react-native';
+import {sendEmail} from '../../common/authApi';
+import {PurpleRoundButton} from '../../components/Button';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import RightArrow from '../../../resources/icon/Arrow';
+import NewsCheckIcon from '../../../resources/icon/NewsCheckIcon';
 
 const HomeFragment = () => {
   const [nickname, setNickname] = useState<string>('익명');
   useEffect(() => {
     async function getNickname() {
-      const nickname = await AsyncStorage.getItem("nickname");
+      const nickname = await AsyncStorage.getItem('nickname');
       if (nickname != null) {
         setNickname(nickname);
       }
     }
     getNickname();
-    
   }, []);
 
   return (
     <ScrollView style={{flex: 1, backgroundColor: '#FFFFFF'}}>
       <View>
-        <View style={{ backgroundColor: '#F7F7F7', marginHorizontal: 12, borderRadius: 20, marginTop: 12}}>
-          <View style={{ borderRadius: 20, marginLeft: 32, marginTop: 32, marginBottom: 32}}><Text style={{fontSize: 22}}>{`${nickname} 님, 안녕하세요!`}</Text></View>
+        <View
+          style={{
+            backgroundColor: '#F7F7F7',
+            marginHorizontal: 12,
+            borderRadius: 20,
+            marginTop: 12,
+          }}>
+          <View
+            style={{
+              borderRadius: 20,
+              marginLeft: 32,
+              marginTop: 32,
+              marginBottom: 32,
+            }}>
+            <Text style={{fontSize: 22}}>
+              <Text style={{fontWeight: 'bold'}}>${nickname}</Text>
+              {` 님, 안녕하세요!`}
+            </Text>
+          </View>
+          <View style={styles.NewsContainer}>
+            <NewsCheckIcon></NewsCheckIcon>
+            <Text style={styles.NewsTitle}>인증 만료일이 3일 남았어요.</Text>
+            <RightArrow style={{color: '#000'}}></RightArrow>
+          </View>
+        </View>
+        <View
+          style={{
+            marginLeft: 24,
+            marginTop: 44,
+            marginRight: 24,
+            marginBottom: 8,
+          }}>
+          <Text style={styles.boardTitle}>고정 게시판</Text>
+          {/* 게시판 글 목록 */}
+          <View style={styles.postSummaryContainer}>
+            <Text style={styles.postSummary}>
+              게시판공백포함15자까지표기해...
+            </Text>
+            <Text style={styles.postTitleSummary}>제목 길이에 맞춰...</Text>
+            <Text style={styles.postNewLabel}>N</Text>
+          </View>
+          <View style={styles.postSummaryContainer}>
+            <Text style={styles.postSummary}>
+              게시판공백포함15자까지표기해...
+            </Text>
+            <Text style={styles.postTitleSummary}>제목 길이에 맞춰...</Text>
+            <Text style={styles.postNewLabel}>N</Text>
+          </View>
+          <View style={styles.postSummaryContainer}>
+            <Text style={styles.postSummary}>
+              게시판공백포함15자까지표기해...
+            </Text>
+            <Text style={styles.postTitleSummary}>제목 길이에 맞춰...</Text>
+            <Text style={styles.postNewLabel}>N</Text>
+          </View>
+          <View style={styles.postSummaryContainer}>
+            <Text style={styles.postSummary}>
+              게시판공백포함15자까지표기해...
+            </Text>
+            <Text style={styles.postTitleSummary}>제목 길이에 맞춰...</Text>
+            <Text style={styles.postNewLabel}>N</Text>
+          </View>
+          {/* 게시판 글 목록 */}
+          <View
+            style={{
+              borderBottomColor: '#F0F0F0',
+              borderBottomWidth: 1,
+              marginBottom: 24,
+              marginTop: 24,
+            }}
+          />
+
+          <Text style={styles.boardTitle}>HOT 게시판</Text>
+          <View style={styles.postSummaryContainer}>
+            <Text style={styles.postSummary}>
+              게시판에 공백 포함 23자 본문을 노출합니다...
+            </Text>
+            <Text style={styles.HOTpostLike}>000</Text>
+            <Text style={styles.HOTpostComment}>000</Text>
+          </View>
         </View>
       </View>
     </ScrollView>
@@ -29,3 +108,49 @@ const HomeFragment = () => {
 };
 
 export default HomeFragment;
+
+const styles = StyleSheet.create({
+  boardTitle: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    marginBottom: 24,
+  },
+  postSummaryContainer: {
+    marginBottom: 15,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  postSummary: {
+    fontSize: 13,
+  },
+  postTitleSummary: {
+    color: '#C4C4C4',
+    fontSize: 13,
+    marginLeft: 16,
+  },
+  postNewLabel: {
+    color: '#FF6060',
+    marginLeft: 8,
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  HOTpostLike: {
+    fontSize: 9,
+    marginLeft: 10,
+  },
+  HOTpostComment: {
+    fontSize: 9,
+    marginLeft: 10,
+  },
+  NewsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 32,
+  },
+  NewsTitle: {
+    fontSize: 13,
+    marginLeft: 10,
+  },
+});
