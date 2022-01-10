3e6aaf562b48a0c8eece08836ab692813f9c08fc
yanwhocodes <ynawhocodes@gmail.com>
2022-01-11 00:53:53 +0900

    #74 EmpytComment Icon 추가

--- resources/icon/EmptyComment.tsx
+++ resources/icon/EmptyComment.tsx
@@ -3,8 +3,8 @@ import Svg, {SvgProps, Path} from 'react-native-svg';
 
 const EmptyComment = (props: any) => (
   <Svg
-    width="10"
-    height="10"
+    width={10}
+    height={10}
     viewBox="0 0 10 10"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
