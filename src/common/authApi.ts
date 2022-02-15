d286cd3f53007d790c37160fe585d15a1f8580cb
bgyoon <gye04056@gmail.com>
2022-02-16 01:51:28 +0900

    #120: 블랙리스트 체크 함수 추가

--- src/common/authApi.ts
+++ src/common/authApi.ts
@@ -1,131 +1,172 @@
-import SignUpRequestDto from "../classes/SignUpRequestDto";
-import client from "./client";
+import SignUpRequestDto from '../classes/SignUpRequestDto';
+import client from './client';
 import {AxiosResponse} from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Major from "../classes/Major";
-import SignUpResponseDto from "../classes/SignUpResponseDto";
-import SignInRequestDto from "../classes/SignInRequestDto";
-import VerificationRequestDto from "../classes/VerificationRequestDto";
+import Major from '../classes/Major';
+import SignUpResponseDto from '../classes/SignUpResponseDto';
+import SignInRequestDto from '../classes/SignInRequestDto';
+import VerificationRequestDto from '../classes/VerificationRequestDto';
 import RegularMemberCheckDto from '../classes/RegularMemberCheckDto';
-import Response from "../classes/Response";
+import Response from '../classes/Response';
 
 export const checkEmailConflict = async (studentId: string) => {
-    try {
-        const response = await client.get<AxiosResponse>("/auth/check-username/" + studentId);
-        if (response.status === 200) {
-            return true;
-        }
+  try {
+    const response = await client.get<AxiosResponse>(
+      '/auth/check-username/' + studentId,
+    );
+    if (response.status === 200) {
+      return true;
     }
-    catch (error) {
-        return false;
-    }
+  } catch (error) {
     return false;
-}
+  }
+  return false;
+};
+
+export const checkBlackList = async (studentId: string) => {
+  try {
+    const response = await client.get<AxiosResponse>(
+      '/auth/check-blacklist/' + studentId,
+    );
+    if (response.status === 200) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+};
 
 export const checkNicknameConflict = async (nickname: string) => {
-    try {
-        const response = await client.get<AxiosResponse>("/auth/check-nickname/" + nickname);
-        if (response.status === 200) {
-            return true;
-        }
+  try {
+    const response = await client.get<AxiosResponse>(
+      '/auth/check-nickname/' + nickname,
+    );
+    if (response.status === 200) {
+      return true;
     }
-    catch (error) {
-        return false;
-    }
+  } catch (error) {
     return false;
-}
+  }
+  return false;
+};
 
 export const getMajorList = async () => {
-    const response = await client.get<Response<Major[]>>("/auth/departments");
-    return response.data.data;
-}
+  const response = await client.get<Response<Major[]>>('/auth/departments');
+  return response.data.data;
+};
 export const register = async (signUpRequestDto: SignUpRequestDto) => {
-    try {
-        const response = await client.post<Response<SignUpResponseDto>>("/auth/signup", signUpRequestDto);
-        console.log(response.data.data);
-        await AsyncStorage.setItem("accessToken", response.data.data.tokenDto.accessToken);
-        await AsyncStorage.setItem("refreshToken", response.data.data.tokenDto.refreshToken);
-        return true;
-    }
-    catch (e) {
-        console.log("여기는 register 함수", e);
-        return false;
-    }
-}
+  try {
+    const response = await client.post<Response<SignUpResponseDto>>(
+      '/auth/signup',
+      signUpRequestDto,
+    );
+    console.log(response.data.data);
+    await AsyncStorage.setItem(
+      'accessToken',
+      response.data.data.tokenDto.accessToken,
+    );
+    await AsyncStorage.setItem(
+      'refreshToken',
+      response.data.data.tokenDto.refreshToken,
+    );
+    return true;
+  } catch (e) {
+    console.log('여기는 register 함수', e);
+    return false;
+  }
+};
 
 export const sendEmail = async () => {
-    try {
-        const accessToken = await AsyncStorage.getItem("accessToken");
-        const response = await client.post<AxiosResponse>("/mail/regular-member", null, {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`
-            }
-        });
-        console.log(response.data);
-        return true;
-    }
-    catch (e) {
-        console.log("여기는 sendEmail 함수", e);
-        return false;
-    }
-}
+  try {
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    const response = await client.post<AxiosResponse>(
+      '/mail/regular-member',
+      null,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    console.log(response.data);
+    return true;
+  } catch (e) {
+    console.log('여기는 sendEmail 함수', e);
+    return false;
+  }
+};
 
 export const checkAuthNumber = async (code: string) => {
-    try {
-        let requestDto: VerificationRequestDto = {code: code};
-        const accessToken = await AsyncStorage.getItem("accessToken");
-        const response = await client.patch<AxiosResponse>("/mail/regular-member-verification", requestDto, {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`
-            }
-        });
-        return 0;
-    }
-    catch (e: any) {
-        console.log("여기는 checkAuthNumber 함수", e.response.data);
-        console.log("여기는 checkAuthNumber 함수", e.response.data.data.attemptCount);
-        return e.response.data.data.attemptCount;
-    }
-}
+  try {
+    let requestDto: VerificationRequestDto = {code: code};
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    const response = await client.patch<AxiosResponse>(
+      '/mail/regular-member-verification',
+      requestDto,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    return 0;
+  } catch (e: any) {
+    console.log('여기는 checkAuthNumber 함수', e.response.data);
+    console.log(
+      '여기는 checkAuthNumber 함수',
+      e.response.data.data.attemptCount,
+    );
+    return e.response.data.data.attemptCount;
+  }
+};
 
 export const login = async (signInRequestDto: SignInRequestDto) => {
-    try {
-        const response = await client.post<Response<SignUpResponseDto>>("/auth/signin", signInRequestDto);
-        console.log('>>>>>>>>', response.data.data);
-        await AsyncStorage.setItem("accessToken", response.data.data.tokenDto.accessToken);
-        await AsyncStorage.setItem("refreshToken", response.data.data.tokenDto.refreshToken);
-        await AsyncStorage.setItem("uuid", response.data.data.uuid);
-        return true;
-    }
-    catch(e: any) {
-        console.log("여기는 login 함수", e.response.status);
-        return false;
-    }
-}
+  try {
+    const response = await client.post<Response<SignUpResponseDto>>(
+      '/auth/signin',
+      signInRequestDto,
+    );
+    console.log('>>>>>>>>', response.data.data);
+    await AsyncStorage.setItem(
+      'accessToken',
+      response.data.data.tokenDto.accessToken,
+    );
+    await AsyncStorage.setItem(
+      'refreshToken',
+      response.data.data.tokenDto.refreshToken,
+    );
+    await AsyncStorage.setItem('uuid', response.data.data.uuid);
+    return true;
+  } catch (e: any) {
+    console.log('여기는 login 함수', e.response.status);
+    return false;
+  }
+};
 
 export const logout = async () => {
-    try {
-        await AsyncStorage.setItem("accessToken", "");
-        return true;
-    }
-    catch (e: any) {
-        console.log("여기는 logout 함수", e.response);
-        return false;
-    }
-}
+  try {
+    await AsyncStorage.setItem('accessToken', '');
+    return true;
+  } catch (e: any) {
+    console.log('여기는 logout 함수', e.response);
+    return false;
+  }
+};
 
 export const checkRegularMember = async () => {
   try {
     const accessToken = await AsyncStorage.getItem('accessToken');
-    const response =  await client.get<Response<RegularMemberCheckDto>>("/user", {
+    const response = await client.get<Response<RegularMemberCheckDto>>(
+      '/user',
+      {
         headers: {Authorization: `Bearer ${accessToken}`},
       },
     );
     console.log(response.data.data);
     return response.data.data.isAuthenticated;
+  } catch (e: any) {
+    console.log('여기는 checkRegularMember 함수', e.response);
+    return false;
   }
-  catch (e: any) {
-      console.log("여기는 checkRegularMember 함수", e.response);
-      return false;
-  }
-}
+};
