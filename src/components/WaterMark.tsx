0e2b9516a326c39088b9c2fcb8059f66c1fb0fdc
yanwhocodes <ynawhocodes@gmail.com>
2022-03-29 00:36:41 +0900

    #103 워터마크에 날짜 추가

--- src/components/WaterMark.tsx
+++ src/components/WaterMark.tsx
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function WaterMark() {
   const [uuid, setUuid] = useState('');
-
+  const [time, setTime] = useState('');
   useEffect(() => {
     async function getUuid() {
       const uuid = await AsyncStorage.getItem('uuid');
@@ -15,18 +15,39 @@ function WaterMark() {
     }
     getUuid();
   });
+  useEffect(() => {
+    let today = new Date();
+    let time = {
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+      date: today.getDate(),
+      hours: today.getHours(),
+      minutes: today.getMinutes(),
+    };
+    let timeString = `${time.year}/${time.month}/${time.date} ${time.hours}:${time.minutes}`;
+    setTime(timeString);
+  }, []);
+  let waterMarkString = '🔮' + uuid + '🔮' + time;
 
   return (
     <View
-      style={{flex: 1, position: 'absolute',left:0, right:0, top:0, zIndex: 1, elevation: 1}}
+      style={{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        zIndex: 1,
+        elevation: 1,
+      }}
       pointerEvents={'none'}>
       <Text
         style={{
-          fontSize: 30,
-          opacity: 0.007,
+          fontSize: 26,
+          opacity: 1.007,
           width: '100%',
         }}>
-        {uuid.repeat(100)}
+        {waterMarkString.repeat(100)}
       </Text>
     </View>
   );
