69090f63707d9fb8c66b836fbd002c8ebabd13fd
yanwhocodes <ynawhocodes@gmail.com>
2022-08-05 14:03:52 +0900

    #177 게시판탭 ios 위아래 회색영역 에러 수정

--- src/screens/fragments/BoardFragment.tsx
+++ src/screens/fragments/BoardFragment.tsx
@@ -106,7 +106,7 @@ export default function BoardFragment({navigation}: Props) {
   
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <>
       <View style={{position: 'absolute', alignItems: 'center', justifyContent: 'center', left: 0, right: 0, top: 0, bottom: 0}}>
         <ActivityIndicator size="large" color={'#A055FF'} animating={isLoading} style={{zIndex: 100}} />
       </View>
@@ -150,6 +150,6 @@ export default function BoardFragment({navigation}: Props) {
           <View style={{height: 36, backgroundColor: '#FFFFFF'}}></View>
         </View>
       </ScrollView>
-    </SafeAreaView>
+      </>
   );
 }
