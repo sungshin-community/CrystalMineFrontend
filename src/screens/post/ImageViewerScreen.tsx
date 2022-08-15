2ae6f329b083150104f334b35591b40797e48d9a
feel-coding <khe628@naver.com>
2022-08-16 01:10:18 +0900

    #190 이미지 로딩중 표시, 아래로 스와이프시 뒤로가기

--- src/screens/post/ImageViewerScreen.tsx
+++ src/screens/post/ImageViewerScreen.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import ImageViewer from "react-native-image-zoom-viewer";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { Text } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 
 interface Props {
   imageUrls: string[];
@@ -18,7 +18,13 @@ const ImageViewerScreen = () => {
         index={route.params.index}
         imageUrls={route.params.imageUrls}
         onCancel={() => {}}
-        enableSwipeDown
+        loadingRender={() => (
+        <View style={{position: 'absolute', alignItems: 'center', justifyContent: 'center', left: 0, right: 0, top: 0, bottom: 0}}>
+          <ActivityIndicator size='large' color={'#A055FF'} animating={true} style={{zIndex: 100}} />
+        </View>)}
+        enableSwipeDown={true}
+        enablePreload={true}
+        onSwipeDown={() => navigation.goBack()}
       />
     </>
   )
