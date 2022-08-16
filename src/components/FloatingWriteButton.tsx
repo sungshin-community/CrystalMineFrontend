cbd795ecfd470ff249ce7b5ceb9b3c846c206486
feel-coding <khe628@naver.com>
2022-08-17 01:18:21 +0900

    #190 플로팅 버튼 hitSlop 추가

--- src/components/FloatingWriteButton.tsx
+++ src/components/FloatingWriteButton.tsx
@@ -17,7 +17,6 @@ const StyledFloatingButton = (props: any) => (
     width="80"
     height="80"
     viewBox="0 0 80 80"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}>
     <Circle cx="40" cy="37" r="30" fill="#A055FF" />
@@ -30,6 +29,7 @@ const StyledFloatingButton = (props: any) => (
 const FloatingWriteButton = ({ onPress }: any) => {
   return (
     <TouchableOpacity
+      hitSlop={{top: 20, right: 20, left: 20, bottom: 20}}
       activeOpacity={0.5}
       onPress={onPress}
       style={styles.touchableOpacityStyle}>
