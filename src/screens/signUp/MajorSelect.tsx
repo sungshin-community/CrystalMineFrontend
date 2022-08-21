80455cab43ee53fc5052c71006d387ba8c8e43f3
yanwhocodes <ynawhocodes@gmail.com>
2022-08-21 20:20:07 +0900

    #177 에러스크린 GNB 수정 "

--- src/screens/signUp/MajorSelect.tsx
+++ src/screens/signUp/MajorSelect.tsx
@@ -186,6 +186,7 @@ export default function MajorSelect({navigation, route}: Props) {
                     ],
                   });
                 }
+                logout();
               }}
             />
           ) : (
