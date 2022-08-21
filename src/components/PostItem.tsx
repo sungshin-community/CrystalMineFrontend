e271d9bfc1fe072cb02a9d51128e41af43de09ca
feel-coding <khe628@naver.com>
2022-08-22 03:36:32 +0900

    #196 불필요한 코드 삭제

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -23,7 +23,7 @@ function PostItem({post, boardType}: Props) {
           <Image style={{ width: 24, height: 24, borderRadius: 12 }}
             source={{uri: post.profileImage}}/>
           <Text style={styles.name}>{post.displayName}</Text>
-          {post.isAnonymous ? <></> : ( post.isOwner && (boardType === 'PUBLIC' ? <SmallOrangeFlag style={{ marginLeft: 5 }}/>: <SmallPurpleFlag style={{marginLeft: 5}}/>))}
+          {!post.isAnonymous && ( post.isOwner && (boardType === 'PUBLIC' ? <SmallOrangeFlag style={{ marginLeft: 5 }}/>: <SmallPurpleFlag style={{marginLeft: 5}}/>))}
         </View>
         <Text style={[styles.textSmall, styles.timeStamp]}>{post.createdAt}</Text>
       </View>
