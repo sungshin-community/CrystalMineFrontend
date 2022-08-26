af040d208734c2512a64a739d2ea3b2d9380e252
yanwhocodes <ynawhocodes@gmail.com>
2022-08-26 23:08:37 +0900

    #198 로그인 비번 토스트 비동기 처리

--- src/screens/signIn/SignInPassword.tsx
+++ src/screens/signIn/SignInPassword.tsx
@@ -193,7 +193,12 @@ export default function SignInPassword({navigation, route}: Props) {
                   password: password,
                 });
                 if (response.status === 401) {
-                  Toast.show('토큰 정보가 만료되어 로그인 화면으로 이동합니다', Toast.SHORT);
+                  setTimeout(function () {
+                    Toast.show(
+                      '토큰 정보가 만료되어 로그인 화면으로 이동합니다',
+                      Toast.SHORT,
+                    );
+                  }, 100);
                   logout();
                   navigation.reset({routes: [{name: 'SplashHome'}]});
                 } else if (getHundredsDigit(response.status) === 2) {
@@ -203,7 +208,12 @@ export default function SignInPassword({navigation, route}: Props) {
                 } else if (response.data.code == 'BLACKLIST_MEMBER') {
                   setIsBlackList(true);
                 } else
-                  Toast.show('알 수 없는 오류가 발생하였습니다.', Toast.SHORT);
+                  setTimeout(function () {
+                    Toast.show(
+                      '알 수 없는 오류가 발생하였습니다.',
+                      Toast.SHORT,
+                    );
+                  }, 100);
               }}
             />
           )}
