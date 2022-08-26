fb0aa5e14213925038f1caccdf736a4efbfaa94c
yanwhocodes <ynawhocodes@gmail.com>
2022-08-26 22:13:03 +0900

    #198 마이페이지>닉네임 변경 토스트 비동기 처리

--- src/screens/mypage/ChangeNickname.tsx
+++ src/screens/mypage/ChangeNickname.tsx
@@ -96,113 +96,118 @@ export default function ChangeNickname({navigation}: Props) {
   return (
     <>
       <KeyboardAvoidingView style={{flex: 1, backgroundColor: '#fff'}}>
-          <ScrollView
-            style={{flex: 1, paddingHorizontal: 24}}>
-            <TextContainer>
-              <NormalOneLineText>
-                새로운 닉네임을 입력해주세요
-              </NormalOneLineText>
-              <Description>공백 없이 10자 이하로 구성해주세요.</Description>
-            </TextContainer>
-            <MiddleInputContainerStyle
-              style={{
-                borderColor: isFocused ? '#A055FF' : '#D7DCE6',
-                marginTop: 52,
-              }}>
-              <TextInput
-                autoFocus={true}
-                style={{
-                  width: '100%',
-                  fontSize: 21,
-                  fontFamily: 'SpoqaHanSansNeo-Regular',
-                  paddingBottom: 7,
-                  color: '#222222',
-                }}
-                onFocus={(e: any) => {
-                  onInputFocus();
-                }}
-                onBlur={(e: any) => {
-                  onInputFocusOut();
-                }}
-                onChangeText={(value: string) => {
-                  setNickname(value.replace(/\s/g, ''));
-                  setIsDuplicate(false);
-                }}
-                placeholder="닉네임"
-                placeholderTextColor="#A0AAB4"
-                keyboardType="default"
-                autoCapitalize="none"
-                autoCorrect={false}
-                returnKeyType="done"
-                selectionColor="#A055FF"
-                value={nickname}
-                maxLength={10}
-              />
-            </MiddleInputContainerStyle>
-            {isDuplicate && (
-              <Text style={styles.errorMessage}>
-                사용할 수 없는 닉네임입니다.
-              </Text>
-            )}
-          </ScrollView>
-          <View
+        <ScrollView style={{flex: 1, paddingHorizontal: 24}}>
+          <TextContainer>
+            <NormalOneLineText>새로운 닉네임을 입력해주세요</NormalOneLineText>
+            <Description>공백 없이 10자 이하로 구성해주세요.</Description>
+          </TextContainer>
+          <MiddleInputContainerStyle
             style={{
-              bottom: isFocused
-                ? Platform.OS == 'ios'
-                  ? keyboardHeight
-                  : 0
-                : 34,
-              justifyContent: 'center',
-              alignItems: 'center',
+              borderColor: isFocused ? '#A055FF' : '#D7DCE6',
+              marginTop: 52,
             }}>
-            {nickname.length >= 1 && isFocused && (
-              <PurpleFullButton
-                text="변경하기"
-                onClick={async () => {
-                  let result: string = await changeNickname(nickname);
-                  if (result === 'UPDATE_NICKNAME_SUCCESS') {
+            <TextInput
+              autoFocus={true}
+              style={{
+                width: '100%',
+                fontSize: 21,
+                fontFamily: 'SpoqaHanSansNeo-Regular',
+                paddingBottom: 7,
+                color: '#222222',
+              }}
+              onFocus={(e: any) => {
+                onInputFocus();
+              }}
+              onBlur={(e: any) => {
+                onInputFocusOut();
+              }}
+              onChangeText={(value: string) => {
+                setNickname(value.replace(/\s/g, ''));
+                setIsDuplicate(false);
+              }}
+              placeholder="닉네임"
+              placeholderTextColor="#A0AAB4"
+              keyboardType="default"
+              autoCapitalize="none"
+              autoCorrect={false}
+              returnKeyType="done"
+              selectionColor="#A055FF"
+              value={nickname}
+              maxLength={10}
+            />
+          </MiddleInputContainerStyle>
+          {isDuplicate && (
+            <Text style={styles.errorMessage}>
+              사용할 수 없는 닉네임입니다.
+            </Text>
+          )}
+        </ScrollView>
+        <View
+          style={{
+            bottom: isFocused
+              ? Platform.OS == 'ios'
+                ? keyboardHeight
+                : 0
+              : 34,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          {nickname.length >= 1 && isFocused && (
+            <PurpleFullButton
+              text="변경하기"
+              onClick={async () => {
+                let result: string = await changeNickname(nickname);
+                if (result === 'UPDATE_NICKNAME_SUCCESS') {
+                  setTimeout(function () {
                     Toast.show(
                       '닉네임이 성공적으로 변경되었습니다.',
                       Toast.SHORT,
                     );
-                    navigation.pop();
-                  } else if (result === 'NICKNAME_DUPLICATION') {
-                    setIsDuplicate(true);
-                  } else {
+                  }, 100);
+                  navigation.pop();
+                } else if (result === 'NICKNAME_DUPLICATION') {
+                  setIsDuplicate(true);
+                } else {
+                  setTimeout(function () {
                     Toast.show('닉네임 변경에 실패하였습니다.', Toast.SHORT);
-                  }
-                }}
-              />
-            )}
+                  }, 100);
+                }
+              }}
+            />
+          )}
 
-            {nickname.length >= 1 && !isFocused && (
-              <PurpleRoundButton
-                text="변경하기"
-                onClick={async () => {
-                  let result: string = await changeNickname(nickname);
-                  if (result === 'UPDATE_NICKNAME_SUCCESS') {
+          {nickname.length >= 1 && !isFocused && (
+            <PurpleRoundButton
+              text="변경하기"
+              onClick={async () => {
+                let result: string = await changeNickname(nickname);
+                if (result === 'UPDATE_NICKNAME_SUCCESS') {
+                  setTimeout(function () {
                     Toast.show(
                       '닉네임이 성공적으로 변경되었습니다.',
                       Toast.SHORT,
                     );
-                    navigation.pop();
-                  } else if (result === 'NICKNAME_DUPLICATION') {
-                    setIsDuplicate(true);
-                  } else {
+                  }, 100);
+                  navigation.pop();
+                } else if (result === 'NICKNAME_DUPLICATION') {
+                  setIsDuplicate(true);
+                } else {
+                  setTimeout(function () {
                     Toast.show('닉네임 변경에 실패하였습니다.', Toast.SHORT);
-                  }
-                }}
-              />
-            )}
+                  }, 100);
+                }
+              }}
+            />
+          )}
 
-            {nickname.length < 1 && isFocused && (
-              <DisabledPurpleFullButton text="다음" />
-            )}
+          {nickname.length < 1 && isFocused && (
+            <DisabledPurpleFullButton text="다음" />
+          )}
 
-            {nickname.length < 1 && !isFocused && (
-              <DisabledPurpleRoundButton text="다음" />
-            )}
-          </View>
+          {nickname.length < 1 && !isFocused && (
+            <DisabledPurpleRoundButton text="다음" />
+          )}
+        </View>
       </KeyboardAvoidingView>
     </>
   );
