05cb11b26618d0d56bcebb4bdb9d12a2871d6552
yanwhocodes <ynawhocodes@gmail.com>
2022-09-13 22:36:02 +0900

    #201 fix: 이용방향 전문) 폰트 적용

--- src/screens/mypage/DirectionAgreeScreen.tsx
+++ src/screens/mypage/DirectionAgreeScreen.tsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {
   ScrollView,
-  View
+  View,
+  Text
 } from 'react-native';
 import Markdown from 'react-native-markdown-display';
 import { AgreementAll, DirectionAgreement } from '../../classes/Agreement';
 import { getAllAgreements } from '../../common/authApi';
 import { getContractGuide } from '../../common/contractApi';
+import { fontRegular } from '../../common/font';
 import WaterMark from '../../components/WaterMark';
 
 export const DirectionAgreeScreen = () => {
@@ -22,9 +24,11 @@ export const DirectionAgreeScreen = () => {
   return (
     <ScrollView style={{ backgroundColor: '#fff',paddingHorizontal: 24, paddingTop: 20}}>
       <WaterMark />
-      <Markdown>
-        {data?.direction.content ? data?.direction.content : ""}
-      </Markdown>
+      <Text style={fontRegular}>
+        <Markdown>
+          {data?.direction.content ? data?.direction.content : ""}
+        </Markdown>
+      </Text>
       <View style={{paddingVertical: 30}}/>
     </ScrollView>
     );
