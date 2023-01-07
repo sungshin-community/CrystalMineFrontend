31b9e6993ccc041db84784cb735db0449ab61230
Leeminhyeong <Leeminhyeong@MacBook-Pro.local>
2023-01-07 22:34:32 +0900

    #213 feat : foreground push notification 추가

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {Alert, AppRegistry} from 'react-native';
 import App from './App';
+import Toast from 'react-native-simple-toast';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
+});
+messaging().onMessage(async remoteMessage => {
+  console.log("Foreground push", remoteMessage);
+  PushNotification.localNotification({
+    message: remoteMessage.notification.body,
+    title: remoteMessage.notification.title
+  });
 });
 
 AppRegistry.registerComponent(appName, () => App);
