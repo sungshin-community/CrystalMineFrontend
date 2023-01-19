2d7f05c458c7bb5c158ecd654e24ac807412fd4d
listenyoon <listenyoon@gmail.com>
2023-01-19 15:26:30 +0900

    #213 fix: 알림 팝업 안뜨던거 해결

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 import PushNotification from 'react-native-push-notification';
 
+PushNotification.createChannel(
+  {
+    channelId: 'crystalminepush', // (required)
+    channelName: 'Special messasge', // (required)
+    channelDescription: 'Notification for special message', // (optional) default: undefined.
+    importance: 4, // (optional) default: 4. Int value of the Android notification importance
+    vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+  },
+  created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+);
+
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
@@ -16,6 +27,7 @@ messaging().onMessage(async remoteMessage => {
   if (Platform.OS === 'ios') {
     //안드로이드에선 중복으로 와서 처리
     PushNotification.localNotification({
+      channelId: 'crystalminepush',
       message: remoteMessage.notification.body,
       title: remoteMessage.notification.title,
     });
