714e7827111238f144cfad11cefac99b861aae80
alreadynyeong <kjw87130@naver.com>
2023-02-04 13:12:33 +0900

    #219 mod: 배너 크기 수정

--- src/components/AdMob.tsx
+++ src/components/AdMob.tsx
@@ -5,7 +5,7 @@ import {BannerAd, BannerAdSize, TestIds} from 'react-native-google-mobile-ads';
 export default function AdMob() {
   return (
     <View style={{alignItems:'center', marginBottom: 16, marginTop: 16}}>
-      <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.BANNER} />
+      <BannerAd unitId={TestIds.BANNER} size="320x70" />
     </View>
   );
 }
