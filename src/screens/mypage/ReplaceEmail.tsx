2c115994b0103934a829c526c4852bc0910c1e2a
alreadynyeong <kjw87130@naver.com>
2023-02-15 17:14:23 +0900

    #221 feat: 대체이메일 존재시 대체이메일 등록 불가능하도록 처리

--- src/screens/mypage/ReplaceEmail.tsx
+++ src/screens/mypage/ReplaceEmail.tsx
@@ -1,10 +1,15 @@
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import DeleteIcon from '../../../resources/icon/DeleteIcon';
-import {PurpleRoundButton} from '../../components/Button';
+import {
+  PurpleRoundButton,
+  DisabledPurpleRoundButton,
+} from '../../components/Button';
 
 export default function ReplaceEmail(navigation) {
-  const mails = ['0000000@sungshin.ac.kr', 'crystal124@naver.com'];
+  const firstmail = '0000000@sungshin.ac.kr';
+  const secondmail = 'crystal124@naver.com';
+  // const secondmail = null;
   return (
     <View style={styles.container}>
       <View style={styles.wrap}>
@@ -16,24 +21,24 @@ export default function ReplaceEmail(navigation) {
         </View>
         <View style={styles.email}>
           {/* 대체 이메일(대표, 삭제가능) */}
-          <View style={styles.repline}>
-            <Text>{mails[1]}</Text>
-            <View style={styles.repview}>
-              <Text style={styles.rep}>대표</Text>
+          {secondmail == null ? (
+            <></>
+          ) : (
+            <View style={styles.repline}>
+              <Text>{secondmail}</Text>
+              <View style={styles.repview}>
+                <Text style={styles.rep}>대표</Text>
+              </View>
             </View>
-          </View>
+          )}
           {/* 원래 이메일 */}
           <View>
-            <Text style={{color: '#CECFD6'}}>{mails[0]}</Text>
+            <Text style={{color: '#CECFD6'}}>{firstmail}</Text>
           </View>
         </View>
         <View style={styles.guide}>
           <Text style={styles.purple}>대체 이메일 등록 이용 안내</Text>
-          <Text style={{
-              color: '#89919A',
-              fontSize: 14,
-              marginTop: 15,
-            }}>
+          <Text style={styles.guidetxt}>
             대체 이메일이 등록되어 있을 경우,{'\n'}
             수정 광산의 공식 메일이 대체 이메일로 전송됩니다.{'\n'}
             {'\n'}
@@ -43,12 +48,16 @@ export default function ReplaceEmail(navigation) {
         </View>
       </View>
       <View style={styles.btn}>
-        <PurpleRoundButton
-          onClick={() => {
-            navigation.navigate('#');
-          }}
-          text="대체 이메일 등록하기"
-        />
+        {secondmail == null ? (
+          <PurpleRoundButton
+            onClick={() => {
+              navigation.navigate('#');
+            }}
+            text="대체 이메일 등록하기"
+          />
+        ) : (
+          <DisabledPurpleRoundButton text="대체 이메일 등록하기" />
+        )}
       </View>
     </View>
   );
@@ -94,6 +103,11 @@ const styles = StyleSheet.create({
     fontSize: 12,
   },
   guide: {bottom: 34},
+  guidetxt: {
+    color: '#89919A',
+    fontSize: 14,
+    marginTop: 15,
+  },
   purple: {
     fontSize: 16,
     color: '#A055FF',
