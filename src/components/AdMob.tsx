6f46a548c9baed49184b78ba28de76376b358582
listenyoon <listenyoon@gmail.com>
2023-03-03 20:51:17 +0900

    #223 mod: 광고 스타일 수정

--- src/components/AdMob.tsx
+++ src/components/AdMob.tsx
@@ -1,11 +1,22 @@
-import {View} from 'react-native';
+import {View, Dimensions} from 'react-native';
 import React from 'react';
 import {BannerAd, TestIds} from 'react-native-google-mobile-ads';
 
 export default function AdMob() {
   return (
-    <View style={{alignItems:'center', marginBottom: 16, marginTop: 16}}>
-      <BannerAd unitId={TestIds.BANNER} size="320x70" />
+    <View
+      style={{
+        alignItems: 'center',
+        marginVertical: 16,
+        marginHorizontal: 24,
+        borderRadius: 10,
+        height: 70,
+        overflow: 'hidden',
+      }}>
+      <BannerAd
+        unitId={TestIds.BANNER}
+        size={`${Math.floor(Dimensions.get('screen').width - 48)}x70`}
+      />
     </View>
   );
 }
