ebb8ae3992a5aef9b0b87c35fa4fa041f62d2203
PangKiYeon <pdrkeamy@gmail.com>
2024-10-02 16:45:05 +0900

    #254 feat: 채팅방 개발 중 스크린 구현

---
+++ src/screens/developScreen/DevelopScreen.tsx
@@ -0,0 +1,31 @@
+import React from 'react'; // 이 줄을 추가하세요
+import {StyleSheet, Text, View} from 'react-native';
+import FixDevelop from '../../../resources/icon/FixDevelop';
+
+export default function DevelopScreen() {
+  return (
+    <View style={styles.container}>
+      <FixDevelop />
+      <Text style={styles.text}>
+        수정광산 팀이{'\n'}열심히 기능을 개발하는 중이에요!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // 크기, 배경 색깔 수정
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    marginTop: 10,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#CECFD6',
+  },
+});
