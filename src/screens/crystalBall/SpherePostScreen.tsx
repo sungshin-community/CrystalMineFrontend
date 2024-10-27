31930de557db90c2741d2fe80e35dad2b794cdbd
PangKiYeon <pdrkeamy@gmail.com>
2024-10-27 17:35:40 +0900

    #254 feat: 궁금해요 글, 댓글 상세 조회 API 구현

--- src/screens/crystalBall/SpherePostScreen.tsx
+++ src/screens/crystalBall/SpherePostScreen.tsx
@@ -1,112 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
 import SphereReplyItem from '../../components/SphereReplyItem';
 import SelectAnswer from '../../components/SelectAnswer';
-
-const data = {
-  post: {
-    content:
-      '닛고웡에 애아솨더리에 닉말암구칠고 언저제서는 탄움고 갤지킨커 교브히쒸. 에히다 개서슴흘홋을 저고게를을 딘듸가 조흔, 가원이다. 니지킨커 교브히쒸. 에히다 개서슴흘홋을 저고게닛고웡에 애아솨더리에 닉말암구칠고 언저제서는 탄움고 갤지킨커 교브히쒸. 에히다 개서슴흘홋을 저고게를을 딘듸가 조흔, 가원이다. 니지킨커 교브히쒸. 에히다 개서슴흘홋을 저고게닛고웡에 애아솨더리에 닉말암구칠고 언저제서는 탄움고 갤지킨커 교브히쒸. 에히다 개서슴흘홋을 저고게를을 딘듸가 조흔, 가원이다. 니지킨커 교브히쒸. 에히다 개서슴흘홋을 저고게 안녕하세요 안녕하세요 안녕하세요',
-    createdAt: '1분전',
-    department: '산업디자인과',
-    likeCount: 1,
-    liked: true,
-    nickname: '익명',
-    profileImage:
-      '/Users/pang-kiyeon/Documents/CrystalMineFrontend/resources/images/GrayPeople.png',
-    ptCommentCount: 3,
-    ptPostId: 1,
-    userJob: '디자인',
-    userYear: 1,
-    isSelected: true,
-    point: 1000,
-    title: '제목입니다.',
-    thumbnail: null,
-    isQuestion: true,
-  },
-};
-
-const replyData = [
-  {
-    content: '이것은 첫 번째 댓글입니다!',
-    authorDepartment: '컴퓨터공학과',
-    authorJob: '학생',
-    authorYear: 2,
-    likeCount: 5,
-    liked: true,
-    nickname: '댓글 작성자1',
-    profileImageUrl: 'https://example.com/profile1.png',
-    ptCommentId: 101,
-    selected: false,
-    replyComments: [
-      {
-        content: '첫 번째 댓글의 대댓글입니다.',
-        authorDepartment: '컴퓨터공학과',
-        authorJob: '조교',
-        authorYear: 1,
-        likeCount: 2,
-        liked: false,
-        nickname: '대댓글 작성자1',
-        profileImageUrl: 'https://example.com/profile_reply1.png',
-        ptCommentId: 201,
-        selected: false,
-      },
-      {
-        content: '또 다른 대댓글입니다.',
-        authorDepartment: '정보통신학과',
-        authorJob: '연구원',
-        authorYear: 3,
-        emoticonUrl: 'https://example.com/emoticon2_reply.png',
-        likeCount: 1,
-        liked: true,
-        nickname: '대댓글 작성자2',
-        profileImageUrl: 'https://example.com/profile_reply2.png',
-        ptCommentId: 202,
-        selected: true,
-      },
-    ],
-  },
-  {
-    content: '두 번째 댓글 예시입니다.',
-    authorDepartment: '정보통신학과',
-    authorJob: '학생',
-    authorYear: 3,
-    likeCount: 8,
-    liked: false,
-    nickname: '댓글 작성자2',
-    profileImageUrl: 'https://example.com/profile2.png',
-    ptCommentId: 102,
-    selected: true,
-    replyComments: [],
-  },
-  {
-    content: '마지막으로 세 번째 댓글입니다.',
-    authorDepartment: '소프트웨어학과',
-    authorJob: '연구원',
-    authorYear: 1,
-    emoticonUrl: 'https://example.com/emoticon3.png',
-    likeCount: 12,
-    liked: true,
-    nickname: '댓글 작성자3',
-    profileImageUrl: 'https://example.com/profile3.png',
-    ptCommentId: 103,
-    selected: false,
-    replyComments: [
-      {
-        content: '세 번째 댓글의 대댓글입니다.',
-        authorDepartment: '소프트웨어학과',
-        authorJob: '학생',
-        authorYear: 2,
-        likeCount: 3,
-        liked: false,
-        nickname: '대댓글 작성자3',
-        profileImageUrl: 'https://example.com/profile_reply3.png',
-        ptCommentId: 203,
-        selected: false,
-      },
-    ],
-  },
-];
+import {
+  getCuriousComment,
+  getCuriousDetail,
+} from '../../common/sphereCuriousApi';
 
 const selectData = {
   content:
@@ -124,32 +23,48 @@ const selectData = {
 interface SpherePostScreenProps {
   route: {
     params: {
+      ptPostId: number;
       isQuestion: boolean;
     };
   };
 }
 
 export default function SpherePostScreen({route}: SpherePostScreenProps) {
-  const {isQuestion} = route.params;
-  // api 수정 필요
-  // navigation 수정 필요
-  const {post} = data;
+  const {ptPostId, isQuestion} = route.params;
+  const [postData, setPostData] = useState<any>(null);
+  const [comments, setComments] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const postResponse = await getCuriousDetail(ptPostId);
+        setPostData(postResponse);
+        const commentResponse = await getCuriousComment(ptPostId);
+        setComments(commentResponse);
+        console.log('궁금해요 글 상세 조회 성공');
+      } catch (error) {
+        console.error('궁금해요 글 상세 조회 실패', error);
+      }
+    };
+
+    fetchData();
+  }, [ptPostId]);
+
   const {
     content,
     createdAt,
     department,
+    images,
     likeCount,
     liked,
     nickname,
     profileImage,
-    ptCommentCount,
     userJob,
     userYear,
     isSelected,
     point,
     title,
-    thumbnail,
-  } = post;
+  } = postData;
 
   return (
     <ScrollView
@@ -290,12 +205,12 @@ export default function SpherePostScreen({route}: SpherePostScreenProps) {
       )}
 
       <View style={{marginBottom: 35}}>
-        {replyData.map((item, index) => (
+        {comments.map((item, index) => (
           <View
             key={index}
             style={[
               {padding: 16, borderBottomColor: '#EFEFF3', borderBottomWidth: 1},
-              index === replyData.length - 1 && {
+              index === comments.length - 1 && {
                 borderBottomWidth: 0,
               },
             ]}>
@@ -303,24 +218,41 @@ export default function SpherePostScreen({route}: SpherePostScreenProps) {
               time={'1분전'}
               reply={item}
               isQuestion={isQuestion}
-              replyCount={item.replyComments.length}
+              replyCount={item.reComments.length}
             />
-            {item.replyComments && item.replyComments.length > 0 && (
+            {item.reComments && item.reComments.length > 0 && (
               <View style={{marginTop: 24}}>
-                {item.replyComments.map((replyComment, idx) => (
-                  <View
-                    style={{
-                      marginBottom:
-                        idx === item.replyComments.length - 1 ? 0 : 20,
-                    }}>
-                    <SphereReplyItem
-                      time={'1분전'}
-                      reply={replyComment}
-                      isReply
-                      isQuestion={isQuestion}
-                    />
-                  </View>
-                ))}
+                {item.reComments.map(
+                  (
+                    reComment: {
+                      content: string;
+                      authorDepartment: string;
+                      authorJob: string;
+                      authorYear: number;
+                      emoticonUrl?: string | null;
+                      likeCount: number;
+                      liked: boolean;
+                      nickname: string;
+                      profileImageUrl: string;
+                      ptCommentId: number;
+                      selected?: boolean;
+                    },
+                    idx: number,
+                  ) => (
+                    <View
+                      style={{
+                        marginBottom:
+                          idx === item.replyComments.length - 1 ? 0 : 20,
+                      }}>
+                      <SphereReplyItem
+                        time={'1분전'}
+                        reply={reComment}
+                        isReply
+                        isQuestion={isQuestion}
+                      />
+                    </View>
+                  ),
+                )}
               </View>
             )}
           </View>
