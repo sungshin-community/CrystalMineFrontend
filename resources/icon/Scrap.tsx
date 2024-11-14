5cd6080090d331cc71a06c478396d71fa018197f
amoeba <113258117+thwjddlqslek@users.noreply.github.com>
2024-11-14 12:19:50 +0900

    #260 fix: NoScrap 아이콘 svg 코드 수정

--- resources/icon/Scrap.tsx
+++ resources/icon/Scrap.tsx
@@ -20,16 +20,15 @@ export default Scrap;
 
 export const NoScrap = (props: any) => (
   <Svg
-    width="14"
-    height="18"
-    viewBox="0 0 14 18"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    width={14}
+    height={18}
+    fill="none"
     {...props}>
     <Path
-      d="M0.76 2.00047V2C0.76 1.30931 1.3191 0.75 2 0.75H12C12.6858 0.75 13.25 1.31421 13.25 2V16.8626L7.29544 14.3106L7 14.184L6.70456 14.3106L0.750711 16.8623L0.76 2.00047Z"
       stroke="#9DA4AB"
-      stroke-width="1.5"
+      strokeWidth={1.5}
+      d="M.76 2C.76 1.31 1.32.75 2 .75h10c.686 0 1.25.564 1.25 1.25v14.863L7.295 14.31 7 14.184l-.295.127L.75 16.862.76 2Z"
     />
   </Svg>
 );
