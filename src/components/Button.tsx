7f3d6b1ac796f6a904b0eb10c679514a27e4b8d9
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-01-31 15:21:10 +0900

    fix: 로그인 버튼 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,8 +22,8 @@ interface ButtonProps {
 const style = StyleSheet.create({
   whiteText: {
     color: '#FFFFFF',
-    fontSize: 14,
-    fontFamily: 'Pretendard-Bold', 
+    fontSize: 17,
+    fontFamily: 'Pretendard-Bold',
   },
   purpleText: {
     color: '#A055FF',
@@ -38,8 +38,8 @@ const style = StyleSheet.create({
   purpleRoundButton: {
     backgroundColor: '#A055FF',
     width: Dimensions.get('window').width - 32,
-    height: 44,
-    borderRadius: 4,
+    height: 56,
+    borderRadius: 10,
   },
   whiteRoundButton: {
     backgroundColor: '#FFFFFF',
