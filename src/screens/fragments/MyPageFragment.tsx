a7c45023fa3144288666c395cfc04513e7275701
doeuni <ehdms1115@naver.com>
2025-02-28 20:48:39 +0900

    fix : 앱버전 수정

--- src/screens/fragments/MyPageFragment.tsx
+++ src/screens/fragments/MyPageFragment.tsx
@@ -772,7 +772,7 @@ const MyPageFragment = ({navigation}: Props) => {
             <View style={styles.menu}>
               <Text style={styles.menuText}>앱 버전</Text>
               <View style={styles.arrowContainer}>
-                <Text style={styles.versionText}>2.0.0</Text>
+                <Text style={styles.versionText}>2.0.2</Text>
               </View>
             </View>
             <TouchableHighlight
